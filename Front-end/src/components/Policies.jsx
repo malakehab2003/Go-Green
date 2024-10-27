22d01255791d6b4b9074c5754b6001abe736226c
malakehab2003 <112804762+malakehab2003@users.noreply.github.com>
2024-10-28 01:46:06 +0300

    fix signup and policies

--- Front-end/src/components/Policies.jsx
+++ Front-end/src/components/Policies.jsx
@@ -7,15 +7,15 @@ const Policies = () => {
     <div className='policiesRoot'>
       <Navbar />
       <main>
-      <div class="form">
-        <p class="policies">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisiLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisiLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisiLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisiLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisiLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisiLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisiLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi</p>
-        <div class="button">
-          <a class="buttonLink" href="/signup"><button class="submit">I agree</button></a>
+      <div className="form">
+        <p className="policies">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisiLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisiLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisiLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisiLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisiLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisiLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisiLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi</p>
+        <div className="button">
+          <a className="buttonLink" href="/signup"><button className="submit">I agree</button></a>
         </div>
       </div>
     </main>
     <footer>
-      <div class="copyrite">
+      <div className="copyrite">
         <p>All rights reserved Ⓒ to 2024</p>
       </div>
     </footer>
--- Front-end/src/components/Signup.jsx
+++ Front-end/src/components/Signup.jsx
@@ -8,64 +8,64 @@ const Signup = () => {
     <div className="signupRoot">
       <Navbar />
       <main>
-      <div class="signupContainer">
+      <div className="signupContainer">
         <h1>CREATE NEW ACCOUNT</h1>
-        <div class="line"></div>
+        <div className="line"></div>
 
-        <div class="nameContainer">
-          <div class="nameIcon">
-            <img src="/name.png" class="nameIcon" alt="name image"></img>
+        <div className="nameContainer">
+          <div className="nameIcon">
+            <img src="/name.png" className="nameIcon" alt="name image"></img>
           </div>
-          <input type="text" placeholder="name" class="nameInput" />
+          <input type="text" placeholder="name" className="nameInput" />
         </div>
 
-        <div class="phoneContainer">
-          <div class="phoneIcon">
-            <img src="/phone.png" class="phoneIcon" alt="phone image"></img>
+        <div className="phoneContainer">
+          <div className="phoneIcon">
+            <img src="/phone.png" className="phoneIcon" alt="phone image"></img>
           </div>
-          <input type="text" placeholder="phone" class="phoneInput" />
+          <input type="text" placeholder="phone" className="phoneInput" />
         </div>
 
-        <div class="whatsappContainer">
-          <div class="whatsappIcon">
-            <img src="/whatsappsignup.png" class="whatsappIcon" alt="whatsapp image"></img>
+        <div className="whatsappContainer">
+          <div className="whatsappIcon">
+            <img src="/whatsappsignup.png" className="whatsappIcon" alt="whatsapp image"></img>
           </div>
-          <input type="text" placeholder="whatsapp" class="whatsappInput" />
+          <input type="text" placeholder="whatsapp" className="whatsappInput" />
         </div>
 
-        <div class="addressContainer">
-          <div class="addressIcon">
-            <img src="/address.png" class="addressIcon" alt="address image"></img>
+        <div className="addressContainer">
+          <div className="addressIcon">
+            <img src="/address.png" className="addressIcon" alt="address image"></img>
           </div>
-          <input type="text" placeholder="address" class="addressInput" />
+          <input type="text" placeholder="address" className="addressInput" />
         </div>
 
 
-        <div class="emailContainer">
-          <div class="emailIcon">
-            <img src="/email.png" class="emailIcon" alt="email image"></img>
+        <div className="emailContainer">
+          <div className="emailIcon">
+            <img src="/email.png" className="emailIcon" alt="email image"></img>
           </div>
-          <input type="text" placeholder="email" class="emailInput" />
+          <input type="text" placeholder="email" className="emailInput" />
         </div>
 
-        <div class="passwordContainer">
-          <div class="passwordIcon">
-            <img src="/password.png" class="passwordIcon" alt="password image"></img>
+        <div className="passwordContainer">
+          <div className="passwordIcon">
+            <img src="/password.png" className="passwordIcon" alt="password image"></img>
           </div>
-          <input type="text" placeholder="password" class="passwordInput" />
+          <input type="text" placeholder="password" className="passwordInput" />
         </div>
 
-        <div class="submitContainer">
-          <div class="submitIcon">
-            <img src="/login.png" alt="submit image" class="submitIcon" />
+        <div className="submitContainer">
+          <div className="submitIcon">
+            <img src="/login.png" alt="submit image" className="submitIcon" />
           </div>
-          <a href="/"><button class="submitButton">Create account</button></a>
+          <a href="/"><button className="submitButton">Create account</button></a>
         </div>
 
       </div>
     </main>
     <footer>
-      <div class="copyrite">
+      <div className="copyrite">
         <p>All rights reserved Ⓒ to 2024</p>
       </div>
     </footer>
